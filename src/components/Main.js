0857d6be8a331d305b865f79dfcf4fbbf676ab17
ah5ive <ah5ive@gmail.com>
2018-11-30 13:03:21 +0800

    testing form

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,7 +55,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="post" action="/success" netlify>
+          <form name="contact" netlify-honeypot="bot-field" method="POST" action="/success/" netlify>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
